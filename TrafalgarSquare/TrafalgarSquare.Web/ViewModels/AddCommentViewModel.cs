7480169f256cd6d8248bbb6cb38da7381ffe28dd
m4r71n85 <martin.tsekov.bg@gmail.com>
2015-05-31 23:34:24 +0300

    add AddCommentViewModel

---
+++ TrafalgarSquare/TrafalgarSquare.Web/ViewModels/AddCommentViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrafalgarSquare.Web.ViewModels
+{
+    using System.ComponentModel.DataAnnotations;
+    using Newtonsoft.Json;
+
+    public class AddCommentViewModel
+    {
+        [Required]
+        public int PostId { get; set; }
+
+        [Required]
+        [MinLength(5, ErrorMessage = "Comment must be at least 5 characters,")]
+        public string Text { get; set; }
+    }
+}
